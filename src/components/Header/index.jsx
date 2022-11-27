f479c0edaa24ecf10086062ed5b220a5aee4568c
shubham-budhiraja-30075199 <shubham.budhiraja@adani.com>
2022-11-27 21:09:13 +0530

    header design done

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from "react";
+import './index.css'
 
 const Header = () => {
   return (
-    <div>Abbe Hello</div>
-  )
-}
+    <>
+      <div className="header-container">
+        <div className="container">
+          <nav className="navigate">
+            <ul className="nav-list nav-list-mobile">
+              <li className="nav-item">
+                <div className="hamburger">
+                  <span className="line line-top"></span>
+                  <span className="line line-btm"></span>
+                </div>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link nav-logo">
+                  Logo
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link nav-cart">
+                  <i class="fa-solid fa-bag-shopping"></i>
+                </a>
+              </li>
+            </ul>
+            <ul className="nav-list nav-list-large">
+              <li className="nav-item nav-item-hidden">
+                <a href="/" className="nav-link nav-logo">
+                  Logo
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  Shop
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  Feedback
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  FAQ
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link nav-search">
+                  <i class="fa-solid fa-magnifying-glass"></i>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link nav-cart">
+                  <i class="fa-solid fa-bag-shopping"></i>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
